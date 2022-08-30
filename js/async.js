89e95812410a8fc5d9211b193df1379005142e14
Iqtidar102011 <iqidarwasif@gmail.comm>
2022-08-30 12:17:44 +0200

    async,try and catch

---
+++ js/async.js
@@ -0,0 +1,30 @@
+const loadUserFetch = () => {
+    const url = 'https://randomuser.me/api/?gender=female'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayUser(data.results[0]))
+        // handle fetch error : try and catch
+        .catch(error => console.log(error))
+}
+
+const loadUserAsync = async () => {
+    try {
+        const url = 'https://randomuser.me/api/?gender=female';
+        const res = await fetch(url);
+        const data = await res.json();
+        displayUser(data.results[0])
+    }
+    catch (error) {
+        console.log(error)
+    }
+    const url = 'https://randomuser.me/api/?gender=female';
+    const res = await fetch(url);
+    const data = await res.json();
+    displayUser(data.results[0])
+
+}
+
+
+const displayUser = user => {
+    console.log(user)
+}
